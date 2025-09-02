322afe53381a185bc8da08ba7bf8e6592b618856
Andri Reinek <andri.reinek@tptlive.ee>
2025-09-02 11:13:45 +0300

    Php lil things

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "Hello World!\n";
+echo ("Hello World\n");
+
+$var = 1;
+echo $var;
+echo 'var is $var';
+echo "\nvar is $var\n";
+
+// TEKSTI LIITMINE .'iga
+
+echo $var . "\n";
+
+$num = 1;
+$num = $num + 1;
+var_dump($num);
